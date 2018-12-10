8ae203282663d71c73b2b606433881accf60a979
Federico Baroni <fbaroni@gmail.com>
2018-12-10 20:03:29 -0300

    calculos totales por medio principales 10

--- R/calculo_totales.R
+++ R/calculo_totales.R
@@ -1,12 +1,35 @@
 source('lectura_datasets.R')
 library(plyr)
 
-total2014 <- sum(pauta2014$total)
-proporcion<- (pauta2014$total/sum(pauta2014$total))*100
+obtener_10_primeros_medios_proporcion <- function(dataframe) {
+  #reemplazo titulo total_con_iva por total para poder ser usado el script desde 2014 a 2018
+  if(colnames(dataframe)[3] == 'total_con_iva'){
+    colnames(dataframe)[3] <- "total"
+  }
+  
+  if(ncol(dataframe) > 10 && colnames(dataframe)[12] == 'total_con_iva'){
+    colnames(dataframe)[12] <- "total"
+  }
+  
+  proporcion <- (dataframe$total/sum(dataframe$total))*100
+  
+  medio_proporcion <- cbind.data.frame(dataframe$medio,proporcion)
+  names(medio_proporcion) <- c("medio", "proporcion")
+  proporcion <- aggregate(medio_proporcion$proporcion, by=list(medio=medio_proporcion$medio), FUN=sum)
+  
+  proporcion_ordenado <- proporcion[order(-proporcion$x),]
+  
+  return(head(proporcion_ordenado, 15))
+}
 
-medio_prop <- cbind.data.frame(pauta2014$medio,proporcion)
-names(medio_prop) <- c("medio", "proporcion")
-prop <- aggregate(medio_prop$proporcion, by=list(medio=medio_prop$medio), FUN=sum)
-newdata <- prop[order(-prop$x),]
+medios2014 <- obtener_10_primeros_medios_proporcion(pauta2014)
+# names(medios2014) <- c('medio2014', 'proporcion2014')
+medios2015 <- obtener_10_primeros_medios_proporcion(pauta2015)
+medios2016_1 <- obtener_10_primeros_medios_proporcion(pauta2016_1)
+medios2016_2 <- obtener_10_primeros_medios_proporcion(pauta2016_2)
+medios2017_1 <- obtener_10_primeros_medios_proporcion(pauta2017_1)
+medios2017_2 <- obtener_10_primeros_medios_proporcion(pauta2017_2)
+medios2018_1 <- obtener_10_primeros_medios_proporcion(pauta2018_1)
 
-print(head(newdata))
+proporcion_10_medios <- data.frame(medios2014, medios2015, medios2016_1, medios2016_2, medios2017_1, medios2018_1)
+print(proporcion_10_medios)
