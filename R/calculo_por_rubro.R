b09051d8bad34fe0d58a6692f3ae4dd9d0659708
Federico Baroni <fbaroni@gmail.com>
2018-12-10 22:13:19 -0300

    calculo por totales y por rubro

--- R/calculo_por_rubro.R
+++ R/calculo_por_rubro.R
@@ -28,14 +28,32 @@ graficar_proporcion_rubro <- function(dataframe) {
   #ordenar
   by_grupo_suma <- by_grupo_suma[order(-by_grupo_suma$x),]
   
-  #grafico de torta
-  pie(by_grupo_suma$x, by_grupo_suma$rubro)
-  
-  #grafico barra ggplot
-  plot(by_grupo_suma$x, by_grupo_suma$rubro)
-  plot(by_grupo_suma$x, by_grupo_suma$rubro, type = "b")
-  p<-ggplot(data=by_grupo_suma, aes(x=rubro, y=x)) + geom_bar(stat="identity")
-  p
+  write.csv(by_grupo_suma, file = "rubro_.csv")
+  # #grafico de torta
+  # pie(by_grupo_suma$x, by_grupo_suma$rubro)
+  # 
+  # #grafico barra ggplot
+  # plot(by_grupo_suma$x, by_grupo_suma$rubro)
+  # plot(by_grupo_suma$x, by_grupo_suma$rubro, type = "b")
+  # p<-ggplot(data=by_grupo_suma, aes(x=rubro, y=x)) + geom_bar(stat="identity")
+  # p
 }
-
-graficar_proporcion_rubro(pauta2018_1)
+obtener_archvo_rubro_proporcion <- function(dataframe, filename) {
+  dataframe <- limpiar_total(dataframe)
+  #Por rubro
+  proporcion <- (dataframe$total/sum(dataframe$total))*100
+  
+  by_rubro <- cbind.data.frame(dataframe$rubro, proporcion)
+  
+  #arreglar nombre
+  names(by_rubro) <- c("rubro", "proporcion")
+  
+  #suma por clase
+  by_grupo_suma <- aggregate(by_rubro$proporcion, by=list(rubro=by_rubro$rubro), FUN=sum)
+  
+  #ordenar
+  by_grupo_suma <- by_grupo_suma[order(-by_grupo_suma$x),]
+  
+  write.csv(by_grupo_suma, file = filename)
+}
+obtener_archvo_rubro_proporcion(pauta2017_1, 'rubro_2017_1.csv')
--- R/calculo_totales.R
+++ R/calculo_totales.R
@@ -1,5 +1,6 @@
 source('lectura_datasets.R')
 library(plyr)
+library(ggplot2)
 
 obtener_10_primeros_medios_proporcion <- function(dataframe) {
   #reemplazo titulo total_con_iva por total para poder ser usado el script desde 2014 a 2018
@@ -22,19 +23,30 @@ obtener_10_primeros_medios_proporcion <- function(dataframe) {
   return(head(proporcion_ordenado, 25))
 }
 
+graficar <- function(dataframe){
+  p<-ggplot(data=dataframe)
+  p
+}
+
 medios2014 <- obtener_10_primeros_medios_proporcion(pauta2014)
-names(medios2014) <- c('medio2014', 'anio2014')
-medios2015 <- obtener_10_primeros_medios_proporcion(pauta2015)
-names(medios2015) <- c('medio2015', 'anio2015')
-medios2016_1 <- obtener_10_primeros_medios_proporcion(pauta2016_1)
-names(medios2016_1) <- c('medios2016_1', 'semestre2016_1')
-medios2016_2 <- obtener_10_primeros_medios_proporcion(pauta2016_2)
-names(medios2016_2) <- c('medios2016_2', 'semestre2016_2')
-medios2017_1 <- obtener_10_primeros_medios_proporcion(pauta2017_1)
-names(medios2017_1) <- c('medios2017_1', 'semestre2017_1')
-medios2017_2 <- obtener_10_primeros_medios_proporcion(pauta2017_2)
-names(medios2017_2) <- c('medios2017_2', 'semestre2017_2')
-medios2018_1 <- obtener_10_primeros_medios_proporcion(pauta2018_1)
-names(medios2018_1) <- c('medios2018_1', 'semestre2018_1')
-proporcion_10_medios <- data.frame(medios2014, medios2015, medios2016_1, medios2016_2, medios2017_1, medios2018_1)
-write.csv(proporcion_10_medios, file = "proporcion_medios.csv")
+names(medios2014) <- c('medio', 'proporcion')
+graficar(medios2014)
+
+# medios2015 <- obtener_10_primeros_medios_proporcion(pauta2015)
+# names(medios2015) <- c('medio', 'proporcion')
+# medios2016_1 <- obtener_10_primeros_medios_proporcion(pauta2016_1)
+# names(medios2016_1) <- c('medio', 'proporcion')
+# medios2016_2 <- obtener_10_primeros_medios_proporcion(pauta2016_2)
+# names(medios2016_2) <- c('medio', 'proporcion')
+# medios2017_1 <- obtener_10_primeros_medios_proporcion(pauta2017_1)
+# names(medios2017_1) <- c('medio', 'proporcion')
+# medios2017_2 <- obtener_10_primeros_medios_proporcion(pauta2017_2)
+# names(medios2017_2) <- c('medio', 'proporcion')
+# medios2018_1 <- obtener_10_primeros_medios_proporcion(pauta2018_1)
+# names(medios2018_1) <- c('medio', 'proporcion')
+# proporcion_10_medios <- data.frame(medios2014, medios2015, medios2016_1, medios2016_2, medios2017_1, medios2018_1)
+# write.csv(proporcion_10_medios, file = "proporcion_medios.csv")
+
+
+
+
